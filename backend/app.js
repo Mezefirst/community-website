535170e9d169df0a42f13419bf81e7b9f587ab24
Mezefirst <98586830+Mezefirst@users.noreply.github.com>
2025-02-25 22:37:08 +0000

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connectDB = require('./config/database');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
 
 const app = express();
 
