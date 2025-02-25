3d9929ed37b26f4e9da64dc658b1d37b20c550c7
Mezefirst <98586830+Mezefirst@users.noreply.github.com>
2025-02-25 21:15:24 +0000

    Creating frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to the Community Website</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
